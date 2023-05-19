22e526adcbe70768ac3c6b63c788ebafd6771501
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-05-19 14:29:18 -0500

    Add components.

---
+++ src/pages/GreetingPage.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useGetMessageQuery } from '../redux/apis/apiSlice'
+
+const GreetingPage = () => {
+  const {
+    data: randomMessage,
+    isLoading,
+    isSuccess,
+    isError,
+    refetch,
+    error
+  } = useGetMessageQuery()
+
+  let content
+
+  if (isLoading) {
+    content = "Loading..."
+  } else if (isSuccess) {
+    content = randomMessage.message
+  } else if (isError) {
+    content = <div>{error.toString()}</div>
+  }
+
+  return (
+    <div>
+      <h3>{content}</h3>
+      <button onClick={refetch}>Get another greeting</button>
+    </div>
+  );  
+}
+
+export default GreetingPage;
